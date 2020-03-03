90d15e4f4cf8db690d4f57ec86a29597811e2fd9
d4n0n_myself <danon.sibaev@gmail.com>
2020-03-03 12:44:40 +0300

    configure postgres context from environment variable

--- IntegrationService.PostgreSQL/ApplicationDbContext.cs
+++ IntegrationService.PostgreSQL/ApplicationDbContext.cs
@@ -1,5 +1,6 @@
-﻿using Microsoft.EntityFrameworkCore;
-using IntegrationService.Models;
+﻿#nullable enable
+using System;
+using Microsoft.EntityFrameworkCore;
 using IntegrationService.Models.Entities;
 
 namespace IntegrationService.PostgreSQL
@@ -12,6 +13,33 @@ namespace IntegrationService.PostgreSQL
 
 		public ApplicationDbContext(DbContextOptions options) : base(options)
 		{
+		}
+
+		protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+		{
+			var connString = GetConnString();
+
+			optionsBuilder.UseNpgsql(connString);
+			base.OnConfiguring(optionsBuilder);
+		}
+
+		private static string GetConnString()
+		{
+			string? connString;
+			try
+			{
+				connString = Environment.GetEnvironmentVariable("PG_CONNSTRING");
+
+				if (string.IsNullOrEmpty(connString))
+					throw new ArgumentException();
+			}
+			catch (Exception e)
+			{
+				Console.Error.WriteLine("Environment variable PG_CONNSTRING is not defined.");
+				throw;
+			}
+
+			return connString;
 		}
 
 		public DbSet<Book> Books { get; set; }
